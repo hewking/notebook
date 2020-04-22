923614daeb0a64f70c1adfdc3d58fa337082556d
hewking <cjh1095358798@gmail.com>
2020-04-22 17:17:18 +0800

    update

--- packages/app-main/src/pages/App.tsx
+++ packages/app-main/src/pages/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from '../logo.svg';
 import './App.css';
-
+import { Layout } from 'antd';
+const { Header, Content } = Layout;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header>
+
+      </Header>
+      <Content style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      }}>
+        <div>
+          Notebook
+        </div>
+      </Content>
     </div>
   );
 }
