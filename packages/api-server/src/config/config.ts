b13cd943f6c23fbdc7a3cfb470e10970243c91e8
hewking <cjh1095358798@gmail.com>
2020-04-23 15:15:22 +0800

    updae database init

--- packages/api-server/src/config/config.ts
+++ packages/api-server/src/config/config.ts
@@ -1,21 +1,15 @@
 const test = {
   database: {
     driver: 'mysql',
-    host: '192.168.5.209',
-    port: 3307,
+    host: '118.25.85.137',
+    port: 3306,
     databasename: 'notebook',
     username: 'root',
-    password: '123456',
+    password: 'cjh123456.',
     maxConnections: 500,
     minConnections: 10,
     idle: 10000,
   },
-  redis: {
-    host: '192.168.5.216',
-    port: 6379,
-    password: '',
-  },
-
 };
 
 
