e17c5fcb677b3a9cf350dad8b9d8082035c085cd
hewking <cjh1095358798@gmail.com>
2020-04-24 15:04:44 +0800

    update

--- packages/api-server/src/reposiories/common/dbContext.ts
+++ packages/api-server/src/reposiories/common/dbContext.ts
@@ -2,17 +2,15 @@ import { Sequelize } from 'sequelize-typescript';
 import config from '../../config/config';
 import path from 'path';
 
-let dbContext: Sequelize = null;
+let dbContext: Sequelize | null = null;
 
 export const buildDbConnection = () => {
   dbContext = new Sequelize({
-    dialect: config.database.driver,
+    dialect: config.database.driver as any,
     host: config.database.host,
-    port: config.database.port,
     database: config.database.databasename,
     username: config.database.username,
     password: config.database.password,
-    operatorsAliases: false,
     logging: console.log,
     pool: {
       max: config.database.maxConnections,
