b3cc3e766655a2b6fb9d61339b21b7cd78caaadb
hewking <cjh1095358798@gmail.com>
2020-04-27 00:12:45 +0800

    add react-router-dom

--- packages/app-main/src/pages/App.tsx
+++ packages/app-main/src/pages/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Router, Route } from 'react-router'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from '../logo.svg';
 import './App.css';
 import { Layout } from 'antd';
 import useHelloWorld from '../hooks/userFetchNote';
 import Text from '../components/text';
+import Note from './note';
 const { Header, Content } = Layout;
 function App() {
   const { content } = useHelloWorld();
@@ -30,4 +36,33 @@ function App() {
   );
 }
 
-export default App;
+const RouteApp = () => {
+  return (<Router>
+    <ul>
+      <li><Link to="./note"> to Note</Link></li>
+      <li><Link to="./app">to Home</Link></li>
+    </ul>
+    <Switch>
+      <Route route="./note">
+        <Note1/>
+      </Route>
+      <Route route="./app">
+        <App1/>
+      </Route>
+    </Switch>
+  </Router>);
+}
+
+function Note1() {
+  return (<div>
+    <h1>Note1</h1>
+  </div>);
+}
+
+function App1(){
+  return (<div>
+    <h1>App1</h1>
+  </div>);
+}
+
+export default RouteApp;
